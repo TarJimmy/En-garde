f729e0e484a51742193ad78aea015a61b5f2dc95
Laetitia <lavognon@yahoo.fr>
2021-06-02 15:20:32 +0200

    Maj fenetre parametres

--- src/view/InterfaceGraphiqueParametres.java
+++ src/view/InterfaceGraphiqueParametres.java
@@ -78,21 +78,21 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 	private JButton btnSaveSettings;
 	
 	//Variables du formulaire
-	static int map;
-	static String nomJoueur1;
-	static String typeJ1;
-	static int posJ1;
-	static String nomJoueur2;
-	static String typeJ2;
-	static int posJ2;
-	static String modeAttaque;
-	static int manches;
-	static int cartes_max;
-	static int cartes1;
-	static int cartes2;
-	static int cartes3;
-	static int cartes4;
-	static int cartes5;
+	private static int map;
+	private static String nomJoueur1;
+	private static String typeJ1;
+	private static int posJ1;
+	private static String nomJoueur2;
+	private static String typeJ2;
+	private static int posJ2;
+	private static String modeAttaque;
+	private static int manches;
+	private static int cartes_max;
+	private static int cartes1;
+	private static int cartes2;
+	private static int cartes3;
+	private static int cartes4;
+	private static int cartes5;
 	
 	private Boolean canEnregistrer;
 	
@@ -290,32 +290,33 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 		/// Section 1 - Terrain
 		titreSectionTerrain = new JLabel("Terrain");
 		titreSectionTerrain.setFont(new Font("Tahoma", Font.BOLD, 14));
-		titreSectionTerrain.setBounds(55, 80, 63, 37);
+		titreSectionTerrain.setBounds(55, 70, 63, 37);
 		contentPane.add(titreSectionTerrain);
 		
 		//// Option - Taille du terrain
 		labelTerrain = new JLabel("Taille du terrain");
-		labelTerrain.setBounds(55, 128, 100, 14);
+		labelTerrain.setBounds(55, 115, 100, 20);
 		contentPane.add(labelTerrain);
 		spinner_map = new JSpinner();
 		spinner_map.setModel(new SpinnerNumberModel(map, 13, 25, 1));
-		spinner_map.setBounds(174, 125, 46, 20);
+		spinner_map.setBounds(174, 115, 46, 20);
 		contentPane.add(spinner_map);
 		
 		//// Options - Escrimeur Gauche
 		labelJ1 = new JLabel("Escrimeur Gauche");
-		labelJ1.setBounds(55, 153, 130, 14);
+		labelJ1.setBounds(55, 140, 130, 20);
 		
 		/////nom du joueur 1
-		txtJoueur1 = new JTextField(nomJoueur1);
+		txtJoueur1 = new JTextField();
 		txtJoueur1.setDocument(new JTextFieldLimit(15));
-		txtJoueur1.setBounds(175, 150, 130, 20);
+		txtJoueur1.setText(nomJoueur1);
+		txtJoueur1.setBounds(175, 140, 130, 20);
 		contentPane.add(txtJoueur1);
 		txtJoueur1.setColumns(10);
 		JLabel warning_J1 = new JLabel("ATTENTION : le nom du joueur ne peut pas \u00EAtre vide");
 		warning_J1.setFont(new Font("Tahoma", Font.BOLD, 11));
 		warning_J1.setForeground(new Color(128, 0, 0));
-		warning_J1.setBounds(175, 178, 300, 14);
+		warning_J1.setBounds(330, 140, 300, 20);
 		warning_J1.setVisible(false);
 		contentPane.add(warning_J1);
 		
@@ -324,31 +325,31 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 		btnRadioJ1_Humain.setActionCommand("HUMAIN");
 		buttonGroup_TypeJ1 = new ButtonGroup();
 		buttonGroup_TypeJ1.add(btnRadioJ1_Humain);
-		btnRadioJ1_Humain.setBounds(332, 149, 70, 23);
+		btnRadioJ1_Humain.setBounds(175, 170, 100, 25);
 		contentPane.add(btnRadioJ1_Humain);
 		btnRadioJ1_Facile = new JRadioButton("IA Facile");
 		btnRadioJ1_Facile.setActionCommand("IA_FACILE");
 		buttonGroup_TypeJ1.add(btnRadioJ1_Facile);
-		btnRadioJ1_Facile.setBounds(408, 150, 86, 23);
+		btnRadioJ1_Facile.setBounds(275, 170, 100, 25);
 		contentPane.add(btnRadioJ1_Facile);
 		btnRadioJ1_Moyenne = new JRadioButton("IA Moyenne");
 		btnRadioJ1_Moyenne.setActionCommand("IA_MOYENNE");
 		buttonGroup_TypeJ1.add(btnRadioJ1_Moyenne);
-		btnRadioJ1_Moyenne.setBounds(490, 150, 100, 23);
+		btnRadioJ1_Moyenne.setBounds(375, 170, 100, 25);
 		contentPane.add(btnRadioJ1_Moyenne);
 		btnRadioJ1_Difficile = new JRadioButton("IA Difficile");
 		btnRadioJ1_Difficile.setActionCommand("IA_DIFFICILE");
 		buttonGroup_TypeJ1.add(btnRadioJ1_Difficile);
-		btnRadioJ1_Difficile.setBounds(592, 150, 86, 23);
+		btnRadioJ1_Difficile.setBounds(475, 170, 100, 25);
 		contentPane.add(btnRadioJ1_Difficile);
 		
 		/////emplacement du joueur 1 au debut de la partie
 		labelPositionJ1 = new JLabel("Position");
-		labelPositionJ1.setBounds(716, 153, 46, 14);
+		labelPositionJ1.setBounds(600, 170, 50, 20);
 		contentPane.add(labelPositionJ1);
 		spinner_positionJ1 = new JSpinner();
 		spinner_positionJ1.setModel(new SpinnerNumberModel(posJ1, 1, map-1, 1));
-		spinner_positionJ1.setBounds(777, 150, 39, 20);
+		spinner_positionJ1.setBounds(660, 170, 40, 20);
 		contentPane.add(spinner_positionJ1);
 		
 		//// Options - Escrimeur Droit
@@ -357,15 +358,16 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 		contentPane.add(labelJ2);
 		
 		/////nom du joueur 2
-		txtJoueur2 = new JTextField(nomJoueur2);
+		txtJoueur2 = new JTextField();
 		txtJoueur2.setDocument(new JTextFieldLimit(15));
+		txtJoueur2.setText(nomJoueur2);
 		txtJoueur2.setColumns(10);
 		txtJoueur2.setBounds(175, 204, 130, 20);
 		contentPane.add(txtJoueur2);
 		JLabel warning_J2 = new JLabel("ATTENTION : le nom du joueur ne peut pas \u00EAtre vide");
 		warning_J2.setFont(new Font("Tahoma", Font.BOLD, 11));
 		warning_J2.setForeground(new Color(128, 0, 0));
-		warning_J2.setBounds(175, 232, 300, 14);
+		warning_J2.setBounds(330, 204, 300, 14);
 		warning_J2.setVisible(false);
 		contentPane.add(warning_J2);
 		
@@ -374,144 +376,144 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 		btnRadioJ2_Humain.setActionCommand("HUMAIN");
 		buttonGroup_TypeJ2 = new ButtonGroup();
 		buttonGroup_TypeJ2.add(btnRadioJ2_Humain);
-		btnRadioJ2_Humain.setBounds(332, 203, 70, 23);
+		btnRadioJ2_Humain.setBounds(175, 237, 100, 25);
 		contentPane.add(btnRadioJ2_Humain);
 		btnRadioJ2_Facile = new JRadioButton("IA Facile");
 		btnRadioJ2_Facile.setActionCommand("IA_FACILE");
 		buttonGroup_TypeJ2.add(btnRadioJ2_Facile);
-		btnRadioJ2_Facile.setBounds(408, 204, 86, 23);
+		btnRadioJ2_Facile.setBounds(275, 237, 100, 25);
 		contentPane.add(btnRadioJ2_Facile);
 		btnRadioJ2_Moyenne = new JRadioButton("IA Moyenne");
 		btnRadioJ2_Moyenne.setActionCommand("IA_MOYENNE");
 		buttonGroup_TypeJ2.add(btnRadioJ2_Moyenne);
-		btnRadioJ2_Moyenne.setBounds(490, 204, 100, 23);
+		btnRadioJ2_Moyenne.setBounds(375, 237, 100, 25);
 		contentPane.add(btnRadioJ2_Moyenne);
 		btnRadioJ2_Difficile = new JRadioButton("IA Difficile");
 		btnRadioJ2_Difficile.setActionCommand("IA_DIFFICILE");
 		buttonGroup_TypeJ2.add(btnRadioJ2_Difficile);
-		btnRadioJ2_Difficile.setBounds(592, 204, 86, 23);
+		btnRadioJ2_Difficile.setBounds(475, 237, 100, 25);
 		contentPane.add(btnRadioJ2_Difficile);
 		
 		/////emplacement du joueur 2 au debut de la partie
 		labelPositionJ2 = new JLabel("Position");
-		labelPositionJ2.setBounds(716, 207, 46, 14);
+		labelPositionJ2.setBounds(600, 237, 50, 20);
 		contentPane.add(labelPositionJ2);
 		spinner_positionJ2 = new JSpinner();
 		spinner_positionJ2.setModel(new SpinnerNumberModel(posJ2, 2, map, 1));
-		spinner_positionJ2.setBounds(777, 204, 39, 20);
+		spinner_positionJ2.setBounds(660, 237, 40, 20);
 		contentPane.add(spinner_positionJ2);
 		contentPane.add(labelJ1);
 		
 		/// Section 2 - Jeu
 		titreSectionJeu = new JLabel("Jeu");
 		titreSectionJeu.setFont(new Font("Tahoma", Font.BOLD, 14));
-		titreSectionJeu.setBounds(55, 295, 46, 14);
+		titreSectionJeu.setBounds(55, 310, 46, 14);
 		contentPane.add(titreSectionJeu);
 		
 		////Option - Mode d'attaque
 		labelModeAttaque = new JLabel("Mode d'attaque :");
-		labelModeAttaque.setBounds(55, 320, 100, 14);
+		labelModeAttaque.setBounds(55, 335, 100, 23);
 		contentPane.add(labelModeAttaque);
 		modeAttaque_Basique = new JRadioButton("Basique");
 		modeAttaque_Basique.setActionCommand("Basique");
 		buttonGroup_ModeAttaque = new ButtonGroup();
 		buttonGroup_ModeAttaque.add(modeAttaque_Basique);
-		modeAttaque_Basique.setBounds(170, 316, 75, 23);
+		modeAttaque_Basique.setBounds(170, 335, 75, 23);
 		contentPane.add(modeAttaque_Basique);
 		modeAttaque_Avance = new JRadioButton("Avanc\u00E9");
 		modeAttaque_Avance.setActionCommand("Avance");
 		buttonGroup_ModeAttaque.add(modeAttaque_Avance);
-		modeAttaque_Avance.setBounds(246, 316, 75, 23);
+		modeAttaque_Avance.setBounds(246, 335, 75, 23);
 		contentPane.add(modeAttaque_Avance);
 		
 		////Option - Nombre de manches
 		labelNbManches = new JLabel("Nombre de manches");
-		labelNbManches.setBounds(55, 360, 130, 14);
+		labelNbManches.setBounds(55, 375, 130, 20);
 		contentPane.add(labelNbManches);
 		spinner_manches = new JSpinner();
 		spinner_manches.setModel(new SpinnerNumberModel(manches, 1, 10, 1));
-		spinner_manches.setBounds(181, 357, 39, 20);
+		spinner_manches.setBounds(181, 375, 39, 20);
 		contentPane.add(spinner_manches);
 		
 		////Option - Nombre maximum de cartes en mains
 		labelCartesMax = new JLabel("Nombre maximum de cartes en mains");
-		labelCartesMax.setBounds(55, 397, 243, 14);
+		labelCartesMax.setBounds(55, 410, 243, 20);
 		contentPane.add(labelCartesMax);
 		spinner_cartesMax = new JSpinner();
 		spinner_cartesMax.setModel(new SpinnerNumberModel(cartes_max, 3, 7, 1));
-		spinner_cartesMax.setBounds(279, 394, 39, 20);
+		spinner_cartesMax.setBounds(279, 410, 39, 20);
 		contentPane.add(spinner_cartesMax);
 		
 		////Option - Nombre de cartes par valeurs
 		labelNbCartesValeurs = new JLabel("Nombre de cartes par valeurs :");
-		labelNbCartesValeurs.setBounds(55, 436, 212, 14);
+		labelNbCartesValeurs.setBounds(55, 450, 212, 14);
 		contentPane.add(labelNbCartesValeurs);
 		
 		/////cartes de valeur 1
 		labelCartes1 = new JLabel("Cartes 1");
-		labelCartes1.setBounds(248, 436, 77, 14);
+		labelCartes1.setBounds(55, 480, 80, 20);
 		contentPane.add(labelCartes1);
 		spinner_cartes1 = new JSpinner();
 		spinner_cartes1.setModel(new SpinnerNumberModel(cartes1, 0, 10, 1));
-		spinner_cartes1.setBounds(301, 433, 39, 20);
+		spinner_cartes1.setBounds(120, 480, 40, 20);
 		contentPane.add(spinner_cartes1);
 		
 		/////cartes de valeur 2
 		labelCartes2 = new JLabel("Cartes 2");
-		labelCartes2.setBounds(363, 436, 74, 14);
+		labelCartes2.setBounds(220, 480, 80, 20);
 		contentPane.add(labelCartes2);
 		spinner_cartes2 = new JSpinner();
 		spinner_cartes2.setModel(new SpinnerNumberModel(cartes2, 0, 10, 1));
-		spinner_cartes2.setBounds(418, 433, 39, 20);
+		spinner_cartes2.setBounds(285, 480, 40, 20);
 		contentPane.add(spinner_cartes2);
 		
 		/////cartes de valeur 3
 		labelCartes3 = new JLabel("Cartes 3");
-		labelCartes3.setBounds(482, 436, 80, 14);
+		labelCartes3.setBounds(385, 480, 80, 20);
 		contentPane.add(labelCartes3);
 		spinner_cartes3 = new JSpinner();
 		spinner_cartes3.setModel(new SpinnerNumberModel(cartes3, 0, 10, 1));
-		spinner_cartes3.setBounds(534, 433, 39, 20);
+		spinner_cartes3.setBounds(450, 480, 40, 20);
 		contentPane.add(spinner_cartes3);
 		
 		/////cartes de valeur 4
 		labelCartes4 = new JLabel("Cartes 4");
-		labelCartes4.setBounds(602, 436, 85, 14);
+		labelCartes4.setBounds(550, 480, 80, 20);
 		contentPane.add(labelCartes4);
 		spinner_cartes4 = new JSpinner();
 		spinner_cartes4.setModel(new SpinnerNumberModel(cartes4, 0, 10, 1));
-		spinner_cartes4.setBounds(656, 433, 39, 20);
+		spinner_cartes4.setBounds(615, 480, 40, 20);
 		contentPane.add(spinner_cartes4);
 		
 		/////cartes de valeur 5
 		labelCartes5 = new JLabel("Cartes 5");
-		labelCartes5.setBounds(725, 436, 79, 14);
+		labelCartes5.setBounds(715, 480, 80, 20);
 		contentPane.add(labelCartes5);
 		spinner_cartes5 = new JSpinner();
 		spinner_cartes5.setModel(new SpinnerNumberModel(cartes5, 0, 10, 1));
-		spinner_cartes5.setBounds(777, 433, 39, 20);
+		spinner_cartes5.setBounds(780, 480, 40, 20);
 		contentPane.add(spinner_cartes5);
 		
 		///Boutons
 		warnEnregistrement = new JLabel("Enregistrement des parametres impossible");
 		warnEnregistrement.setFont(new Font("Tahoma", Font.BOLD, 11));
 		warnEnregistrement.setForeground(new Color(128, 0, 0));
-		warnEnregistrement.setBounds(70, 517, 300, 40);
+		warnEnregistrement.setBounds(70, 540, 300, 40);
 		warnEnregistrement.setVisible(false);
 		contentPane.add(warnEnregistrement);
 		btnSaveSettings = Button("Enregistrer comme parametres par defaut");
 		btnSaveSettings.addActionListener(new AdaptateurCommande(this.controle, "sauvePara", canEnregistrer));
-		btnSaveSettings.setBounds(55, 517, 266, 37);
+		btnSaveSettings.setBounds(55, 540, 266, 37);
 		contentPane.add(btnSaveSettings);
 		
 		JButton btnRestoreSettings = Button("Retablir les parametres par defaut");
 		btnRestoreSettings.addActionListener(new AdaptateurCommande(this.controle, "restaurePara"));
-		btnRestoreSettings.setBounds(331, 517, 266, 37);
+		btnRestoreSettings.setBounds(331, 540, 266, 37);
 		contentPane.add(btnRestoreSettings);
 		
 		JButton btnPlay = Button("MENU");
 		btnPlay.addActionListener(new AdaptateurCommande(this.controle, "menu"));
-		btnPlay.setBounds(634, 517, 195, 37);
+		btnPlay.setBounds(634, 540, 195, 37);
 		contentPane.add(btnPlay);
 		
 		//setRadioButtons
@@ -551,7 +553,7 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 		
 		//fenetreParametres.add(para_fond);
 		fenetreParametres.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		fenetreParametres.setBounds(100, 100, 900, 635);
+		fenetreParametres.setBounds(100, 100, 900, 650);
 		fenetreParametres.setResizable(false);
 		fenetreParametres.setVisible(true);
 	}
