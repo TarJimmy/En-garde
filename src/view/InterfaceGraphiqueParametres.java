c05bf2a578d6210330ba491a6da9ddc6fad8f85e
Ayasetsu <lavognon@yahoo.fr>
2021-06-02 15:32:58 +0200

    Maj parametres

--- src/view/InterfaceGraphiqueParametres.java
+++ src/view/InterfaceGraphiqueParametres.java
@@ -300,6 +300,7 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 		spinner_map = new JSpinner();
 		spinner_map.setModel(new SpinnerNumberModel(map, 13, 25, 1));
 		spinner_map.setBounds(174, 115, 46, 20);
+		((JSpinner.DefaultEditor) spinner_map.getEditor()).getTextField().setEditable(false);
 		contentPane.add(spinner_map);
 		
 		//// Options - Escrimeur Gauche
@@ -344,12 +345,13 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 		contentPane.add(btnRadioJ1_Difficile);
 		
 		/////emplacement du joueur 1 au debut de la partie
-		labelPositionJ1 = new JLabel("Position");
+		labelPositionJ1 = new JLabel("Départ :");
 		labelPositionJ1.setBounds(600, 170, 50, 20);
 		contentPane.add(labelPositionJ1);
 		spinner_positionJ1 = new JSpinner();
 		spinner_positionJ1.setModel(new SpinnerNumberModel(posJ1, 1, map-1, 1));
 		spinner_positionJ1.setBounds(660, 170, 40, 20);
+		((JSpinner.DefaultEditor) spinner_positionJ1.getEditor()).getTextField().setEditable(false);
 		contentPane.add(spinner_positionJ1);
 		
 		//// Options - Escrimeur Droit
@@ -395,12 +397,13 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 		contentPane.add(btnRadioJ2_Difficile);
 		
 		/////emplacement du joueur 2 au debut de la partie
-		labelPositionJ2 = new JLabel("Position");
+		labelPositionJ2 = new JLabel("Départ :");
 		labelPositionJ2.setBounds(600, 237, 50, 20);
 		contentPane.add(labelPositionJ2);
 		spinner_positionJ2 = new JSpinner();
 		spinner_positionJ2.setModel(new SpinnerNumberModel(posJ2, 2, map, 1));
 		spinner_positionJ2.setBounds(660, 237, 40, 20);
+		((JSpinner.DefaultEditor) spinner_positionJ2.getEditor()).getTextField().setEditable(false);
 		contentPane.add(spinner_positionJ2);
 		contentPane.add(labelJ1);
 		
@@ -433,6 +436,7 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 		spinner_manches = new JSpinner();
 		spinner_manches.setModel(new SpinnerNumberModel(manches, 1, 10, 1));
 		spinner_manches.setBounds(181, 375, 39, 20);
+		((JSpinner.DefaultEditor) spinner_manches.getEditor()).getTextField().setEditable(false);
 		contentPane.add(spinner_manches);
 		
 		////Option - Nombre maximum de cartes en mains
@@ -442,6 +446,7 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 		spinner_cartesMax = new JSpinner();
 		spinner_cartesMax.setModel(new SpinnerNumberModel(cartes_max, 3, 7, 1));
 		spinner_cartesMax.setBounds(279, 410, 39, 20);
+		((JSpinner.DefaultEditor) spinner_cartesMax.getEditor()).getTextField().setEditable(false);
 		contentPane.add(spinner_cartesMax);
 		
 		////Option - Nombre de cartes par valeurs
@@ -456,6 +461,7 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 		spinner_cartes1 = new JSpinner();
 		spinner_cartes1.setModel(new SpinnerNumberModel(cartes1, 0, 10, 1));
 		spinner_cartes1.setBounds(120, 480, 40, 20);
+		((JSpinner.DefaultEditor) spinner_cartes1.getEditor()).getTextField().setEditable(false);
 		contentPane.add(spinner_cartes1);
 		
 		/////cartes de valeur 2
@@ -465,6 +471,7 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 		spinner_cartes2 = new JSpinner();
 		spinner_cartes2.setModel(new SpinnerNumberModel(cartes2, 0, 10, 1));
 		spinner_cartes2.setBounds(285, 480, 40, 20);
+		((JSpinner.DefaultEditor) spinner_cartes2.getEditor()).getTextField().setEditable(false);
 		contentPane.add(spinner_cartes2);
 		
 		/////cartes de valeur 3
@@ -474,6 +481,7 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 		spinner_cartes3 = new JSpinner();
 		spinner_cartes3.setModel(new SpinnerNumberModel(cartes3, 0, 10, 1));
 		spinner_cartes3.setBounds(450, 480, 40, 20);
+		((JSpinner.DefaultEditor) spinner_cartes3.getEditor()).getTextField().setEditable(false);
 		contentPane.add(spinner_cartes3);
 		
 		/////cartes de valeur 4
@@ -483,6 +491,7 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 		spinner_cartes4 = new JSpinner();
 		spinner_cartes4.setModel(new SpinnerNumberModel(cartes4, 0, 10, 1));
 		spinner_cartes4.setBounds(615, 480, 40, 20);
+		((JSpinner.DefaultEditor) spinner_cartes4.getEditor()).getTextField().setEditable(false);
 		contentPane.add(spinner_cartes4);
 		
 		/////cartes de valeur 5
@@ -492,6 +501,7 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 		spinner_cartes5 = new JSpinner();
 		spinner_cartes5.setModel(new SpinnerNumberModel(cartes5, 0, 10, 1));
 		spinner_cartes5.setBounds(780, 480, 40, 20);
+		((JSpinner.DefaultEditor) spinner_cartes5.getEditor()).getTextField().setEditable(false);
 		contentPane.add(spinner_cartes5);
 		
 		///Boutons
@@ -538,6 +548,8 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 				} else {
 					spinner_positionJ2.setModel(new SpinnerNumberModel((int) spinner_positionJ2.getValue(), 2, (int) spinner_map.getValue(), 1));
 				}
+				((JSpinner.DefaultEditor) spinner_positionJ1.getEditor()).getTextField().setEditable(false);
+				((JSpinner.DefaultEditor) spinner_positionJ2.getEditor()).getTextField().setEditable(false);
 			}
 		});
 		spinner_positionJ1.addChangeListener(new ChangeListener() {
@@ -548,6 +560,8 @@ public class InterfaceGraphiqueParametres implements Runnable, Observateur {
 				} else {
 					spinner_positionJ2.setModel(new SpinnerNumberModel((int) spinner_positionJ2.getValue(), (int) spinner_positionJ1.getValue()+1, (int) spinner_map.getValue(), 1));
 				}
+				((JSpinner.DefaultEditor) spinner_positionJ1.getEditor()).getTextField().setEditable(false);
+				((JSpinner.DefaultEditor) spinner_positionJ2.getEditor()).getTextField().setEditable(false);
 			}
 		});
 		
